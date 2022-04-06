4e18a616bb998649630b3c566985e26e1b73b758
astral-g <gfield991@gmail.com>
2022-04-06 11:13:53 +0100

    [WIP]: Backend functionaliy complete - needs CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,20 +62,82 @@ const IndexPage = ({data}) => {
     // Nest sub-directories within their parent
     const nestDirectory = (directory) => {
         const child = fileMap.find(dir => dir.parent === directory.name);
-        
+
         if(child === undefined) {
             return
         }
-    
+
         fileMap.forEach( node => {
             if(node.parent === child.name) {
                 nestDirectory(child)
             }
         })
-        let currentIdx = fileMap.map(idx => idx.name).indexOf(directory.name);
-        let childIdx = fileMap.map(idx => idx.name).indexOf(child.name);
+
+        const currentIdx = fileMap.map(idx => idx.name).indexOf(directory.name);
+        const childIdx = fileMap.map(idx => idx.name).indexOf(child.name);
 
         fileMap[currentIdx].data = fileMap[currentIdx].data.concat(fileMap.splice(childIdx, 1));
+    };
+
+    const outputDirectory = (directory) => {
+        const child = directory.data.find(dir => dir.parent === directory.name);
+
+        if(child === undefined) {
+            return (
+                <details>
+                    <summary>{directory.name}</summary>
+                    <ul>
+                        {directory.data.map((node, i) =>
+                            <li key={i}>
+                                <a 
+                                    className="govuk-link" 
+                                    href={node.frontmatter.slug}
+                                    onClick={(event) => handleClick(event, node.html)}
+                                >
+                                    <h2 className="govuk-heading-l">{node.frontmatter.title}</h2>
+                                </a>
+                                <h4 className="govuk-heading-s">
+                                    <time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time>
+                                </h4>
+                                <hr className="govuk-section-break govuk-section-break--l govuk-section-break--visible" />
+                            </li>
+                        )}
+                    </ul>
+                </details>
+            )
+        }
+
+        return ( 
+            <details>
+                <summary>{directory.name}</summary>
+                <ul>
+                    {directory.data.map((node, i) => {
+                        const key = Object.keys(node)[2]
+                        if(key === "data") {
+                            return (
+                                outputDirectory(node)
+                            )
+                        } else {
+                            return (
+                                <li key={i}>
+                                    <a 
+                                        className="govuk-link" 
+                                        href={node.frontmatter.slug}
+                                        onClick={(event) => handleClick(event, node.html)}
+                                    >
+                                        <h2 className="govuk-heading-l">{node.frontmatter.title}</h2>
+                                    </a>
+                                    <h4 className="govuk-heading-s">
+                                        <time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time>
+                                    </h4>
+                                    <hr className="govuk-section-break govuk-section-break--l govuk-section-break--visible" />
+                                </li>
+                            )
+                        }
+                    })}
+               </ul>
+            </details>
+        )
     };
 
     data.allFile.nodes.map(({ relativeDirectory, childMarkdownRemark }) => {
@@ -85,19 +147,33 @@ const IndexPage = ({data}) => {
         return createDirectories(relativeDirectory, childMarkdownRemark)
     });
 
-    console.log(fileMap)
-
     fileMap.forEach(dir => {
         nestDirectory(dir);
     });
 
-    const outputDirectories = () => {
-
-    }
-
-    console.log(fileMap)
     return (
-        <h1>test</h1>
+        <main>
+            <title>Home Page</title>
+            <h1 className="govuk-heading-l">Hippo Documentation Scaffholding</h1>
+
+            <div className="govuk-grid-row">
+                <div className="govuk-grid-column-one-third">
+                    <nav id="side-menu">
+                        <ol className="govuk-list" >
+                            {
+                                fileMap.map(dir => outputDirectory(dir))
+                            }
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <div 
+                className="govuk-grid-column-two-thirds" 
+                id="content-area"
+                dangerouslySetInnerHTML={{ __html: content}}
+            >
+            </div>
+        </main>
     )
 }
 
